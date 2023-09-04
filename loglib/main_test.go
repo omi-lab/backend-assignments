949e96b73d93cf748608dd9789ede86ebcc59cc7
hugo vantighem <hugo.vantighem@gmail.com>
2023-09-04 17:47:02 +0200

    use env vars

--- loglib/main_test.go
+++ loglib/main_test.go
@@ -7,10 +7,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestEmit(t *testing.T) {
+func TestEmit(t *testing.T) { // TODO remove or make it IT
 	logrus.SetLevel(logrus.DebugLevel)
 
-	err := Emit(LogEntry{Action: "test"})
+	err := Emit(LogEntry{Action: "test"}, BrokerParams{})
 
 	assert.NoError(t, err)
 
